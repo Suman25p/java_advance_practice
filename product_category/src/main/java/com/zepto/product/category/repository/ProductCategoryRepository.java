fd4826bb478d838c77f302a7ac9f328865c0d897
Suman <pasisuman2511@gmail.com>
2026-04-23 17:52:16 +0530

    Hibernate lazy-Eager Loading

---
+++ product_category/src/main/java/com/zepto/product/category/repository/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.zepto.product.category.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.zepto.product.category.entity.Category;
+
+public interface ProductCategoryRepository extends CrudRepository<Category, Integer>{
+
+}
